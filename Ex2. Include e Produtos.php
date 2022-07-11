deff7da939cc8e0724058174d5084e81c6fe67cb
Kely100 <109049770+Kely100@users.noreply.github.com>
2022-07-10 23:24:39 -0300

    Create Ex2. Include e Produtos.php

---
+++ Ex2. Include e Produtos.php
@@ -0,0 +1,12 @@
+<?php
+class Produtos {
+    public $id;
+    public $nome;
+    public $valorProduto;
+    public $quantidade;
+    public $valorCalculado;
+
+
+public function calcularValor(){
+ $this->valorCalculado=$this->valorProduto*$this->quantidade;
+}
